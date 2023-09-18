b45638ef4dc6e861ae8cf83f4813a1797029e9e7
sagarkhatridk <sagarkhatridk@gmail.com>
2023-09-18 19:26:54 +0530

    sweet alert

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -7,7 +7,7 @@ const addMsg = () => {
     let subject = document.getElementById("subject").value;
     let message = document.getElementById("msg").value;
 
-    console.log({ name, email, subject, message });
+  
 
     // ***************************************************************************************************
     fetch(`https://backend-production-e796.up.railway.app/api/sendmail/`, {
@@ -18,7 +18,6 @@ const addMsg = () => {
       body: JSON.stringify({ name, email, message, subject }),
     });
 
-
     fetch(`https://backend-production-e796.up.railway.app/api/sendmsg/`, {
       method: "POST",
       headers: {
@@ -27,19 +26,17 @@ const addMsg = () => {
       body: JSON.stringify({ name, email, subject, message }),
     });
 
-    document.getElementById("sendconf").innerHTML = "<b>Your message has been send. We will contant you soon!<b>";
-    setTimeout(() => {
-      document.getElementById("name").value = "";
-      document.getElementById("email").value = "";
-      document.getElementById("subject").value = "";
-      document.getElementById("msg").value = "";
-    }, 1000);
+    swal("Success", "You information has been received!", "success");
+    
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("msg").value = "";
+    
 
     setTimeout(() => {
-      
-      document.getElementById("sendconf").innerHTML = ""
+      document.getElementById("sendconf").innerHTML = "";
     }, 5000);
-
   });
 };
 
